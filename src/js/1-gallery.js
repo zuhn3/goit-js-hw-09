4f82a517a35ee09cdf69af4d8f7d78ee6b49503f
zuhn3 <aop1603@gmail.com>
2025-03-31 17:33:55 +0100

    add simplelightbox

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -36,52 +36,21 @@ const images = [
     original:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
     description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
+  }
 ];
 
 const container = document.querySelector('.gallery');
 container.insertAdjacentHTML('beforeend', createMarkup(images));
+
 function createMarkup(array) {
   return array
     .map(
       item => `
   <li class="gallery-item">
-  <a class="gallery-link" href="${item.original}">
-    <img
-      class="gallery-image"
-      src="${item.preview}"
-      alt="${item.description}"
-    />
-  </a>
-</li>
+    <a class="gallery-link" href="${item.original}">
+      <img class="gallery-image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>
 `
     )
     .join('');
